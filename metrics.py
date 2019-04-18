ede8032851c28f311d8c08b283c8428a451c3595
Rajashan <raja.zaker@gmail.com>
2019-04-19 01:06:36 +0200

    exact match metric

---
+++ metrics.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import numpy as np
+
+# wei zhengchang de seq2seq, bie de dou you tese de wenti
+
+trg = pd.read_csv("seq2seq_trg.csv", sep=",",header = None)
+trg = trg.T 
+
+trg = trg[0].str.replace(" ","")
+trg = trg[~trg.astype(float).isnull()]
+trg = [s[1:] for s in trg]
+
+
+output = pd.read_csv("seq2seq_output.csv",sep = ",",header = None)
+output = output.T
+output[~output.isnull()]
+output = output[0].str.replace(" ","")
+output = output[~output.astype(float).isnull()]
+output = [s[1:] for s in output] 
+
+
+
+print("debug ahoy!")
+
+loss = 0
+
+for row in range(len(output)):
+    if output[row] == trg[row]:
+        None
+    else:
+        loss += 1 
+        
+match = loss / len(trg)
